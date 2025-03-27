c7b184f0e0add13f34c57e6aa8f848827092198d
rajashree <srajashree104@gmail.com>
2025-03-27 14:46:02 +0530

    14th

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,7 +7,15 @@ function App() {
 
       <Sidebar />
       <MainContentStyled>
-        
+        <div className="lines">
+          <div className="line-1"></div>
+          <div className="line-2"></div>
+          <div className="line-3"></div>
+
+          <div className="line-4"></div>
+
+
+        </div>
       </MainContentStyled>
     </div>
   );
@@ -18,7 +26,19 @@ const MainContentStyled = styled.main`
 position: relative;
 margin-left: 16.3rem;
 min-height: 100vh;
-background-color: red;
+.lines{
+position: absolute;
+min-height: 100vh;
+width: 100%;
+display: flex;
+justify-content: space-evenly;
+.line-1, .line-2, .line-3, .line-4{
+  width:1px;
+  min-height: 100vh;
+  background-color: var(--border-color);
+
+}
+}
 `;
 export default App
 
---
+++ portfolio/src/pages/HomePage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function HomePage() {
+  return (
+    <HomePageStyle>
+
+    </HomePageStyle>
+  )
+}
+
+export default HomePage
+
+
+const HomePageStyle = styled.header `
+width: 100%;
+height: 100vh;
+
+
+`;
+
